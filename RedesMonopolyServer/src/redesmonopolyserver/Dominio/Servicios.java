7c6fea6b7da8e87817abaa425099606defa2e5d9
kamgmc <kamgmc@gmail.com>
2018-06-09 15:00:46 -0400

    Se cambio el nombre por servicio

--- RedesMonopolyServer/src/redesmonopolyserver/Dominio/Servicios.java
+++
@@ -1,9 +0,0 @@
-
-package redesmonopolyserver.Dominio;
-
-public class Servicios {
-    private int propietario;
-    private String nombre;
-    private int unServicio;
-    private int dosServicios;
-}
